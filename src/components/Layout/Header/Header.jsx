cba38b38e20319ecaab59f33f5de11aac17c7e59
Abdul Wahab <102620981+wahab315@users.noreply.github.com>
2023-04-03 00:05:53 +0500

    new code

---
+++ src/components/Layout/Header/Header.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+import classes from "./Header.module.css";
+import mealsImage from "../.././../assests/meals.jpg";
+const Header = () => {
+  return (
+    <>
+      <header className={classes.header}>
+        <h1>React Meals</h1>
+        <button>Cart</button>
+      </header>
+      <div className={classes["main-image"]}>
+        <img src={mealsImage} alt="Meal Image" />
+      </div>
+    </>
+  );
+};
+
+export default Header;
